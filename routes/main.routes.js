37daaf30b80a985c74f7fa6b0df3a1b0e1e188b6
RomaFek <rqvyzz@gmail.com>
2023-06-02 10:50:33 +0300

    mainpp

--- routes/main.routes.js
+++ routes/main.routes.js
@@ -1,18 +1,21 @@
-const router = require("express").Router();
-const MainPage = require("../components/MainPage");
-const { User } = require("../db/models");
+const router = require('express').Router();
+const MainPage = require('../components/MainPage');
+const { User } = require('../db/models');
+const { Theme } = require('../db/models');
 
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const name = req.body.nameID;
     const findname = await User.findOne({ where: { name: name } });
     if (findname == null) {
       const result = await User.create({ name: name, score: 0 });
     }
+    const ThemeAll = await Theme.findAll({ raw: true });
     res.send(
       res.renderComponent(MainPage, {
-        title: "Reg page",
+        title: 'Reg page',
         name: req.body.nameID,
+        thems: ThemeAll
       })
     );
   } catch ({ message }) {
