eb421026d12a412e48e8779f17933529d62ee463
pu99r <iiigor.mik@gmail.com>
2023-06-02 13:45:35 +0300

    dev20

--- components/Cardtest.jsx
+++ components/Cardtest.jsx
@@ -1,30 +1,35 @@
 const React = require("react");
 const Layout = require("./Layout");
 
-function CardTest({ title, name, id, thems, themeqa, qustionid = 0 }) {
+function CardTest({ title, name, id, thems, themeqa, qustionid }) {
   return (
     <Layout title={title} name={name} met>
       <div class="card">
         <div class="card-header">{thems[id].name}</div>
         <div class="card-body">
           <h5 class="card-title">
-            Текс вопроса: {themeqa[qustionid].question}
+            Вопрос {qustionid + 1}: {themeqa[qustionid].question}
           </h5>
           <div>
-            <form id="form" action="/main" method="post">
+            <form id="form" action="/theme" method="post">
               <input
                 name="answer"
                 type="text"
                 placeholder="Введите ответ: "
               ></input>
-              <button type="submit" className="btn-next btn-primary">
-                Узнать ответ
-              </button>
+
               <button type="click" className="btn-next btn-primary">
                 Next
               </button>
-              <div>{themeqa[qustionid].answer}</div>
+              <input name="id" type="hidden" value={id} />
+              <input name="questionid" type="hidden" value={qustionid} />
             </form>
+            <div>
+              <button type="submit" className="btn-next btn-primary">
+                Узнать ответ
+              </button>
+              <div >{themeqa[qustionid].answer}</div>
+            </div>
           </div>
         </div>
       </div>
--- routes/theme.routes.js
+++ routes/theme.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const { Theme } = require("../db/models");
 const { QuestionAnswer } = require("../db/models");
+const { User } = require("../db/models");
 const CardTest = require("../components/Cardtest");
 
 router.get("/:id", async (req, res) => {
@@ -15,6 +16,26 @@ router.get("/:id", async (req, res) => {
         id: id,
         thems: ThemeAll,
         themeqa: ThemeQA,
+        qustionid: 0,
+      })
+    );
+  } catch ({ message }) {
+    res.json(message);
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    let count = Number(req.body.questionid) + 1
+    const ThemeAll = await Theme.findAll({ raw: true });
+    const ThemeQA = await QuestionAnswer.findAll({ raw: true });
+    res.send(
+      res.renderComponent(CardTest, {
+        title: "Quiz",
+        id: req.body.id,
+        thems: ThemeAll,
+        themeqa: ThemeQA,
+        qustionid: count,
       })
     );
   } catch ({ message }) {
